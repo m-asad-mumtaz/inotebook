8ccfeeffa80d2d09d8bab6bd5a2f38895aecfab4
Asad Mumtaz <asad@realtypaas.com>
2022-10-04 15:01:09 +0500

    Login & Signup complete

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,107 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User')
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// Create a User using: POST "/api/auth". Does not require Authentication
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body)
+const JWT_SECRET = 'As@disgoodb0y';
+
+//ROUTE 1: Create a User using: POST "/api/auth/createuser". Does not require Login
+router.post('/createuser', [
+    body('name', 'Enter a valid Name').isLength({ min: 3 }),
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password must be 8 or more characters').isLength({ min: 8 })
+], async (req, res) => {
+    // If there are errors, return Bad request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        // Check whether the user with this email already exists or not
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" })
+        }
+
+        // Generating Salt and adding it to password hash
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+        //Create a new user
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass,
+        });
+
+        // Generating JSON Web Token
+        const data = {
+            user: {
+                id: user.id,
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken: authToken });
+
+        // res.json(user)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internel Server Error!");
+    }
 });
 
+//ROUTE 2: Authenticate a User using: POST "/api/auth/login". Does not require Login
+router.post('/login', [
+    body('email', 'Enter a valid Email!').isEmail(),
+    body('password', 'Password Cannot be Blank!').exists(),
+], async (req, res) => {
+    // If there are errors, return Bad request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        // Check whether the user with this email already exists or not
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" })
+        }
+        // Comparing password
+        const passCompare = await bcrypt.compare(password, user.password);
+        if (!passCompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" })
+        }
+
+        const data = {
+            user: {
+                id: user.id,
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken: authToken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internel Server Error!");
+    }
+});
+
+//ROUTE 3: Get Loggedin User details using: POST "/api/auth/getuser". Login required 
+router.post('/getuser', [
+    body('email', 'Enter a valid Email!').isEmail(),
+    body('password', 'Password Cannot be Blank!').exists(),
+], async (req, res) => {
+    try {
+        userId = "todo";
+        const user = await User.findById(userId).select("-password");
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internel Server Error!");
+    }
+});
 module.exports = router;
--- iNoteBook Readme.txt
+++ iNoteBook Readme.txt
@@ -6,3 +6,5 @@ iNoteBook Project:
 4) then install express & mongoose in backend: npm install express mongoose
 5) you can install "thunder client" extension in VS Code to test the APIs
 6) Install Express Validator to validate the inputs: npm i --save express-validator
+7) Install BCryptJS to secure the passwords: npm i bcryptjs
+8) Install JSON Web Token (JWT): npm i jsonwebtoken
